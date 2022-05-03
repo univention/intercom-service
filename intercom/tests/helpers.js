f0778deb8d3963ca4fb95a708b37bf0d29a42a10
Andreas Nolden <nolden.extern@univention.de>
2022-05-03 08:08:59 +0200

    backup before switching to new test env

--- intercom/tests/helpers.js
+++ intercom/tests/helpers.js
@@ -2,20 +2,27 @@ const {expect} = require("@playwright/test");
 
 
 const logintoOxFake = async (page) => {
-    await page.goto("https://localhost:8000/conference");
+    // TODO: Configurable...
+    await page.goto("http://localhost:8000/conference");
 
-    // TODO: Make this optional
-    await page.locator('text=●Accept').click();
+    // ucs login or kc login
+    if (process.env.MODE === "remote") {
+        await page.locator('text=●Accept').click();
 
-    await page.locator('input[name="username"]').fill(process.env.TESTUSER);
+        await page.locator('input[name="username"]').fill(process.env.TESTUSER);
 
-    await page.locator('input[name="password"]').fill(process.env.TESTPASSWORD);
+        await page.locator('input[name="password"]').fill(process.env.TESTPASSWORD);
 
-    // TODO: Make this conditional
-    await page.locator('button:has-text("Login")').click();
+        // TODO: Make this conditional
+        await page.locator('button:has-text("Login")').click();
 
-    // local
-    //await page.locator('input:has-text("Sign In")').click();
+    } else {
+        await page.locator('input[name="username"]').fill(process.env.TESTUSER);
+
+        await page.locator('input[name="password"]').fill(process.env.TESTPASSWORD);
+
+        await page.locator('input:has-text("Sign In")').click();
+    }
 
     await expect(page.locator('text=Imagine Creating a Videoconference')).toBeVisible()
 
