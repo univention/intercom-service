389e6dbf0e0bdad777bc525bc9c84e3664125a86
Jaime Conde Segovia <conde-segovia.extern@univention.de>
2022-08-16 13:55:29 +0200

    feat: docker image same as app version and public

--- update-appcenter-test.sh
+++ update-appcenter-test.sh
@@ -6,7 +6,7 @@
 set -e
 set -x
 
-APP_VERSION="5.0/intercom-service=0.0.1-ucs2"
+APP_VERSION="5.0/intercom-service=0.9"
 
 selfservice () {
 	local uri="https://provider-portal.software-univention.de/appcenter-selfservice/univention-appcenter-control"
