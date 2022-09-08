87c73299b6c21c32dff54f077053db95dac0b3ce
Jaime Conde Segovia <conde-segovia.extern@univention.de>
2022-09-08 14:25:36 +0200

    enh(cleanup)

--- intercom/backend.js
+++ intercom/backend.js
@@ -3,111 +3,143 @@
  */
 
 // Ask providers if there's any ouath in the stack. Oauth as in "I have an access token provided by keycloak and can do stuff via REST, ..."
-require('dotenv').config({ path: './.env.prod' });
+require("dotenv").config({ path: "./.env.prod" });
 // console.log(process.env)
-const express = require('express')
+const express = require("express");
 var app = express();
-app.set('view engine', 'ejs');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const { fetchMatrixToken, fetchToken, stripIntercomCookies, massageCors } = require('./helpers')
-const { auth, requiresAuth, attemptSilentLogin, claimEquals } = require('express-openid-connect')
-const csrfDSC = require('express-csrf-double-submit-cookie')
-const jwt_decode = require("jwt-decode")
-const cookieParser = require('cookie-parser')
+app.set("view engine", "ejs");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const {
+  fetchMatrixToken,
+  fetchToken,
+  stripIntercomCookies,
+  massageCors,
+} = require("./helpers");
+const {
+  auth,
+  requiresAuth,
+  attemptSilentLogin,
+  claimEquals,
+} = require("express-openid-connect");
+const csrfDSC = require("express-csrf-double-submit-cookie");
+const jwt_decode = require("jwt-decode");
+const cookieParser = require("cookie-parser");
 
-const { createClient } = require('redis');
-const RedisStore = require('connect-redis')(auth);
+const { createClient } = require("redis");
+const RedisStore = require("connect-redis")(auth);
 
-var cors = require('cors')
-
+var cors = require("cors");
 
 var corsOptions = {
-    methods: "GET,POST,PUT,DELETE,PROPFIND,MKCOL",
-    credentials: true,
-    // TODO: can we drop Authorization?
-    exposedHeaders: ["etag", "dav", "Content-Security-Policy", "Location", "Authorization", "depth", "content-type", "ocs-apirequest"],
-    origin: new RegExp(process.env.ORIGIN_REGEX),
-}
+  methods: "GET,POST,PUT,DELETE,PROPFIND,MKCOL",
+  credentials: true,
+  // TODO: can we drop Authorization?
+  exposedHeaders: [
+    "etag",
+    "dav",
+    "Content-Security-Policy",
+    "Location",
+    "Authorization",
+    "depth",
+    "content-type",
+    "ocs-apirequest",
+  ],
+  origin: new RegExp(process.env.ORIGIN_REGEX),
+};
 
 const csrfProtection = csrfDSC({ cookie: { sameSite: "none", secure: true } });
 
 app.use(express.urlencoded());
-let redisClient = createClient({ legacyMode: true, url: `redis://default:${process.env.REDIS_PASSWORD}@redis-intercom:6379` });
-redisClient.connect().catch(console.error)
+let redisClient = createClient({
+  legacyMode: true,
+  url: `redis://default:${process.env.REDIS_PASSWORD}@redis-intercom:6379`,
+});
+redisClient.connect().catch(console.error);
 
 app.use(
-    auth({
-        // TODO; move to environ
-        issuerBaseURL: process.env.ISSUER_BASE_URL ?? `${process.env.KEYCLOAK_URL}/auth/realms/${process.env.REALM_NAME}`,
-        baseURL: process.env.BASE_URL,
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        authRequired: false,
-        secret: process.env.SECRET,
-        idpLogout: true,
-        authorizationParams: {
-            response_type: 'code',
-            scope: 'openid'
-        },
-        session: {
-            store: new RedisStore({ client: redisClient }),
-        },
-        afterCallback: async (req, res, session, decodedState) => {
-            // TODO: Add some kind of error handling, if tokens can't be fetched the user should see an error message of some sort
-            try {
-                var ret = {}
-                // fetch token for ox
-                // TODO: check also if it's valid
-                if (!('ox_access_token' in session)) {
-                    ret.ox_access_token = await fetchToken(session.access_token, `${process.env.OX_AUDIENCE}`)
-                }
-
-                const token = jwt_decode(session.id_token)
-                let uid = token['entryuuid']
-
-                if (!uid) {
-                    console.log("Sorry can't find the preferred username/uuid, maybe the mapping is missing?")
-                }
-
-                if (!('matrix_access_token' in session)) {
-                    console.log("fetching matrix token")
-                    ret.matrix_access_token = await fetchMatrixToken(uid)
-                }
-            } catch (error) {
-                console.log("Error fetching Tokens: " + error)
-            }
-            return { ...session, ...ret }
+  auth({
+    // TODO; move to environ
+    issuerBaseURL:
+      process.env.ISSUER_BASE_URL ??
+      `${process.env.KEYCLOAK_URL}/auth/realms/${process.env.REALM_NAME}`,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    authRequired: false,
+    secret: process.env.SECRET,
+    idpLogout: true,
+    authorizationParams: {
+      response_type: "code",
+      scope: "openid",
+    },
+    session: {
+      store: new RedisStore({ client: redisClient }),
+    },
+    afterCallback: async (req, res, session, decodedState) => {
+      // TODO: Add some kind of error handling, if tokens can't be fetched the user should see an error message of some sort
+      try {
+        var ret = {};
+        // fetch token for ox
+        // TODO: check also if it's valid
+        if (!("ox_access_token" in session)) {
+          ret.ox_access_token = await fetchToken(
+            session.access_token,
+            `${process.env.OX_AUDIENCE}`
+          );
         }
-    }))
 
+        const token = jwt_decode(session.id_token);
+        let uid = token["entryuuid"];
 
-app.use(cors(corsOptions))
+        if (!uid) {
+          console.log(
+            "Sorry can't find the preferred username/uuid, maybe the mapping is missing?"
+          );
+        }
+
+        if (!("matrix_access_token" in session)) {
+          console.log("fetching matrix token");
+          ret.matrix_access_token = await fetchMatrixToken(uid);
+        }
+      } catch (error) {
+        console.log("Error fetching Tokens: " + error);
+      }
+      return { ...session, ...ret };
+    },
+  })
+);
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
-app.use(csrfProtection)
+app.use(csrfProtection);
 app.use((req, res, next) => {
-    if (req.cookies && 'appSession' in req.cookies && req.appSession
-        && 'session_state' in req.appSession) {
-        redisClient.set(req.appSession['session_state'], req.cookies['appSession'])
-    }
-    next()
-})
+  if (
+    req.cookies &&
+    "appSession" in req.cookies &&
+    req.appSession &&
+    "session_state" in req.appSession
+  ) {
+    redisClient.set(req.appSession["session_state"], req.cookies["appSession"]);
+  }
+  next();
+});
 
 app.post("/backchannel-logout", async (req, res) => {
-    // TODO: Actually check the Token, Verify the signature, ...
-    const token = jwt_decode(req.body.logout_token)
-    redisClient.get(token['sid'], function(err, session_id) {
-        redisClient.del("sess:" + session_id)
-        redisClient.del(token['sid'])
-        res.send("Done")
-    });
-})
+  // TODO: Actually check the Token, Verify the signature, ...
+  const token = jwt_decode(req.body.logout_token);
+  redisClient.get(token["sid"], function (err, session_id) {
+    redisClient.del("sess:" + session_id);
+    redisClient.del(token["sid"]);
+    res.send("Done");
+  });
+});
 
 /**
  * Just a simple Endpoint to check if the service is there and for CORS testing
  */
-app.get('/', function(req, res) {
-    res.send("<p>Hello</p>")
-})
+app.get("/", function (req, res) {
+  res.send("<p>Hello</p>");
+});
 
 /**
  * @name /nob/
@@ -115,25 +147,35 @@ app.get('/', function(req, res) {
  * Proxy for the Nordeck Bot (or just the plain Matrix UserInfo Service in testing).
  * Adds the proper Authorization Header
  */
-app.use('/nob', requiresAuth(), csrfProtection.validate, createProxyMiddleware({
-    target: process.env.NORDECK_URL, logLevel: 'debug', changeOrigin: true,
-    pathRewrite: { '^/nob': '', }, secure: false,
+app.use(
+  "/nob",
+  requiresAuth(),
+  csrfProtection.validate,
+  createProxyMiddleware({
+    target: process.env.NORDECK_URL,
+    logLevel: "debug",
+    changeOrigin: true,
+    pathRewrite: { "^/nob": "" },
+    secure: false,
     onProxyReq: function onProxyReq(proxyReq, req, res) {
-        stripIntercomCookies(proxyReq)
-        // TODO: Build Switch for Nordeck Live Mode
-        // Example headers.set('authorization', `MX-Identity ${btoa(JSON.stringify(t))}`);
-        // or  proxyReq.setHeader('authorization', `Bearer ${matrix_access_token}`);
+      stripIntercomCookies(proxyReq);
+      // TODO: Build Switch for Nordeck Live Mode
+      // Example headers.set('authorization', `MX-Identity ${btoa(JSON.stringify(t))}`);
+      // or  proxyReq.setHeader('authorization', `Bearer ${matrix_access_token}`);
 
-        if (req.appSession.matrix_access_token) {
-            proxyReq.setHeader('authorization', `Bearer ${req.appSession.matrix_access_token}`);
-        }
+      if (req.appSession.matrix_access_token) {
+        proxyReq.setHeader(
+          "authorization",
+          `Bearer ${req.appSession.matrix_access_token}`
+        );
+      }
     },
-    onProxyRes: function(proxyRes, req, res) {
-        // TODO: Matrix seems to be specific with it's headers, we have to decide whether to steamroll or to massage...
-        massageCors(req, proxyRes, corsOptions.origin)
-    }
-}))
-
+    onProxyRes: function (proxyRes, req, res) {
+      // TODO: Matrix seems to be specific with it's headers, we have to decide whether to steamroll or to massage...
+      massageCors(req, proxyRes, corsOptions.origin);
+    },
+  })
+);
 
 /**
  * @name /fs/
@@ -143,22 +185,30 @@ app.use('/nob', requiresAuth(), csrfProtection.validate, createProxyMiddleware({
  * @example PROPFIND http://ic.p.test/fs/remote.php/dav/files/usera1/Photos
  *
  */
-app.use('/fs', requiresAuth(), createProxyMiddleware({
-    target: process.env.NC_URL, logLevel: 'debug', changeOrigin: true,
+app.use(
+  "/fs",
+  requiresAuth(),
+  createProxyMiddleware({
+    target: process.env.NC_URL,
+    logLevel: "debug",
+    changeOrigin: true,
     pathRewrite: {
-        '^/fs': ''
+      "^/fs": "",
     },
     onProxyReq: function onProxyReq(proxyReq, req, res) {
-        // TODO: Service takes pretty much any token which is not good
-        stripIntercomCookies(proxyReq)
-        proxyReq.setHeader('authorization', `Bearer ${req.appSession.ox_access_token}`);
-        console.log(proxyReq)
+      // TODO: Service takes pretty much any token which is not good
+      stripIntercomCookies(proxyReq);
+      proxyReq.setHeader(
+        "authorization",
+        `Bearer ${req.appSession.ox_access_token}`
+      );
+      console.log(proxyReq);
     },
-    onProxyRes: function(proxyRes, req, res) {
-        massageCors(req, proxyRes, corsOptions.origin)
-    }
-}
-))
+    onProxyRes: function (proxyRes, req, res) {
+      massageCors(req, proxyRes, corsOptions.origin);
+    },
+  })
+);
 
 /**
  * @name "/portal.json (without silly quotes for jsdoc)"
@@ -167,17 +217,31 @@ app.use('/fs', requiresAuth(), createProxyMiddleware({
  * Adds the proper Authorization Header
  */
 // TODO: https://127.0.0.1/univention/portal/navigation.json?lang=de_DE -H "Authorization: Basic username:MyPortalSecretFromBMIUXAnsibleHostINI"
-app.use('/navigation.json', requiresAuth(), createProxyMiddleware({
-    target: process.env.PORTAL_URL, logLevel: 'debug', changeOrigin: true,
-    pathRewrite: { '^/navigation.json': '/univention/portal/navigation.json' },
+app.use(
+  "/navigation.json",
+  requiresAuth(),
+  createProxyMiddleware({
+    target: process.env.PORTAL_URL,
+    logLevel: "debug",
+    changeOrigin: true,
+    pathRewrite: { "^/navigation.json": "/univention/portal/navigation.json" },
     onProxyReq: function onProxyReq(proxyReq, req, res) {
-        stripIntercomCookies(proxyReq)
-        proxyReq.setHeader('Authorization', 'Basic ' + btoa(jwt_decode(req.appSession.id_token)['phoenixusername'] + ':' + process.env.PORTAL_API_KEY));
-    }, onProxyRes: function(proxyRes, req, res) {
-        massageCors(req, proxyRes, corsOptions.origin)
-    }
-}))
-
+      stripIntercomCookies(proxyReq);
+      proxyReq.setHeader(
+        "Authorization",
+        "Basic " +
+          btoa(
+            jwt_decode(req.appSession.id_token)["phoenixusername"] +
+              ":" +
+              process.env.PORTAL_API_KEY
+          )
+      );
+    },
+    onProxyRes: function (proxyRes, req, res) {
+      massageCors(req, proxyRes, corsOptions.origin);
+    },
+  })
+);
 
 /**
  * @name /silent
@@ -187,11 +251,14 @@ app.use('/navigation.json', requiresAuth(), createProxyMiddleware({
  *
  * Reports the Session Status via window.postmessage (JSON: {"loggedIn": true})
  */
-app.get('/silent', attemptSilentLogin(), (req, res) => {
-    // TODO: Do proper postMessage reporting
-    const sessionStatus = ('access_token' in req.appSession)
-    console.log(`Silent login, logged in ${sessionStatus}`)
-    res.render("pages/silent", { sessionStatus, csrftoken: req.cookies['_csrf_token'] })
+app.get("/silent", attemptSilentLogin(), (req, res) => {
+  // TODO: Do proper postMessage reporting
+  const sessionStatus = "access_token" in req.appSession;
+  console.log(`Silent login, logged in ${sessionStatus}`);
+  res.render("pages/silent", {
+    sessionStatus,
+    csrftoken: req.cookies["_csrf_token"],
+  });
 });
 
 /**
@@ -199,13 +266,13 @@ app.get('/silent', attemptSilentLogin(), (req, res) => {
  * @desc returns the uuid of the logged in user
  */
 app.get("/uuid", requiresAuth(), (req, res) => {
-    // TODO: wrong field, use username?
-    let entryUUID = jwt_decode(req.appSession.id_token)['entryuuid']
-    res.send(entryUUID)
-})
+  // TODO: wrong field, use username?
+  let entryUUID = jwt_decode(req.appSession.id_token)["entryuuid"];
+  res.send(entryUUID);
+});
 
-var server = app.listen(process.env.PORT, function() {
-    var host = server.address().address
-    var port = server.address().port
-    console.info("Intercom app listening at http://%s:%s", host, port)
-})
+var server = app.listen(process.env.PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.info("Intercom app listening at http://%s:%s", host, port);
+});
