db9a0ea5f162f98e11bde547a8f9ab20c1577c38
Jaime Conde Segovia <conde-segovia.extern@univention.de>
2022-12-14 12:20:25 +0100

    fix(logging): message formatting

--- intercom/app.js
+++ intercom/app.js
@@ -191,5 +191,5 @@ app.use(
 var server = app.listen(process.env.PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
-  logger.info("Intercom app listening at http://%s:%s", host, port);
+  logger.info(`Intercom app listening at http://${host}:${port}`);
 });
